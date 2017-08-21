843f2ea084c082b82a925729bcdc373fa32b5651
Tom Runia <tomrunia@gmail.com>
2017-08-21 14:08:56 +0200

    small rewrite of count videos using dataset object

--- test_seg_benchmark/count_videos.py
+++ test_seg_benchmark/count_videos.py
@@ -6,47 +6,37 @@ import numpy as np
 from rep_test_benchmark import prepare_network
 from bench_classify_online import load_and_count_video
 
+from count_dataset.quva_count import QUVACountDataset
+import cortex.count.experiments
 
-def count_quva_dataset(dataset_path):
+def count_entire_quva_dataset(dataset_path):
 
     # Prepare the CNN in Theano
     test_set_x, test_set_y, shared_test_set_y, valid_ds, classify, batchsize = \
         prepare_network()
 
-    video_files = glob.glob(os.path.join(dataset_path, "videos", "*.mp4"))
-    video_files.sort()
+    dataset = QUVACountDataset(dataset_path)
 
-    num_videos = len(video_files)
-    count_results = np.zeros(num_videos, np.int32)
+    # Initialize structure for storing results
+    cnt_pred = np.zeros(dataset.num_examples, np.float32)
+    cnt_true = np.zeros(dataset.num_examples, np.float32)
 
-    for i, video_file in enumerate(video_files):
-
-        video_name, _ = os.path.splitext(os.path.basename(video_file))
-        ann_file = os.path.join(dataset_path, "annotations", "count_ann_1", video_name + ".pkl")
-        if os.path.exists(ann_file):
-            ann = pickle.load(open(ann_file, 'rb'))
-            gt_count = ann['count']
-        else:
-            gt_count = "unknown"
-
-        print(gt_count)
-        continue
-
-        print("#"*60)
-        print("Video {}/{} - {}".format(i, num_videos, video_name))
-        print("  True Count: {}".format(gt_count))
-
+    while dataset.has_next():
+        example = dataset.next_example()
         # Actual counting of the video
-        count_results[i] = load_and_count_video(video_file, classify, test_set_x, batchsize)
+        cnt_pred[example.index] = load_and_count_video(example.video_path, classify, test_set_x, batchsize)
+        cnt_true[example.index] = example.rep_count
+        print("Video {}. True Count = {}, Predict Count = {}"
+              .format(example.name, example.rep_count, cnt_pred[example.index]))
 
-        print("  Predict Count: {}".format(count_results[i]))
-
-
-    return count_results
+    return cnt_true, cnt_pred
 
 if __name__ == "__main__":
 
-    quva_dataset = "/home/trunia1/data/VideoCountingDatasetClean/QUVACount_Segments/"
-    results_file = os.path.join(quva_dataset, "results", "levywolf_online_count.npy")
-    count_results = count_quva_dataset(quva_dataset)
-    #np.save(results_file, count_results)
+    # Analyze videos
+    quva_dataset = "/home/trunia1/data/VideoCountingDataset/QUVACount_Segments/"
+    cnt_true, cnt_pred = count_entire_quva_dataset(quva_dataset)
+
+    # Save results
+    results_path = os.path.join(quva_dataset, "results", "LevyWolf")
+    cortex.count.experiments.write_experiment(cnt_pred, cnt_true, results_path)
