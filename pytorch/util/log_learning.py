a8deca401f87d0c01a923592032fc8f03ebaeccd
Atsushi Kayama <a.shika829@gmail.com>
2018-01-03 14:28:14 +0900

    frist run of trainNet

--- pytorch/util/log_learning.py
+++ pytorch/util/log_learning.py
@@ -4,5 +4,5 @@
 def log_learning(epoch, steps, modelname, loss, args, D_precision=None):
     text = "EPOCH : {0}, step : {1}, {2} : {3}, precision : {4}".format(epoch, steps, modelname, loss, D_precision)
     print(text)
-    with open('{}/Learning_Log.txt'.format(args.save_dir),'a') as f:
+    with open('{}/Learning_Log.txt'.format(args.savedir),'a') as f:
         f.write("{}\n".format(text))
