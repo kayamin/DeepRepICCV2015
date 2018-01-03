2044d92b1240159a6fb7413686aba3e0fe365473
Atsushi Kayama <a.shika829@gmail.com>
2018-01-03 13:28:44 +0900

    frist run of trainNet

--- pytorch/trainNet/rep_train_main.py
+++ pytorch/trainNet/rep_train_main.py
@@ -19,8 +19,8 @@ import pdb
 
 sys.path.append('../')
 from layers import RepetitionCountingNet
-from util/Dataset_loader import Dataset_loader
-from util/log_learning import log_learning
+from util.Dataset_loader import Dataset_loader
+from util.log_learning import log_learning
 
 if __name__=="__main__":
 
