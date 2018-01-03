b1fcf219201cbb0c3583b4b02b2cb943bb460e09
Atsushi Kayama <a.shika829@gmail.com>
2018-01-03 15:22:46 +0900

    frist run of trainNet

--- pytorch/trainNet/rep_train_main.py
+++ pytorch/trainNet/rep_train_main.py
@@ -135,6 +135,7 @@ def train_rep(D_model, trainfilename_df, validfilename_df, args):
             count_pred = D_model(batch_VideoBlocks)
             precision = calc_precision(count_pred, batch_labels)
             Loss_pred = losss_criterion(count_pred, batch_labels)
+            pdb.set_trace()
 
             # calcurate L1, L2 norm
             for W in D_model.parameters():
